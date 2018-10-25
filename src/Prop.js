916a99e5d555a905e6c16b628b0d222d8f77c206
rabbit <rabbitmeow886@gmail.com>
2018-10-25 16:20:38 +0800

    prettify Prop.js

--- src/Prop.js
+++ src/Prop.js
@@ -1,15 +1,10 @@
 import Store from "rako"
 import React from "react"
 
-const $$recalcs = Symbol('recalcs')
-const $$renderer = Symbol('renderer')
-const $$value = Symbol('value')
+const $$private = Symbol ? Symbol('private') : '(private)'
 
 class Renderer {
   constructor(store) {
-    if (!(store instanceof Store)) {
-      throw new TypeError('Expected store to be an instance of Store.')
-    }
     this.listeners = []
     this.instances = []
     store.subscribe(this.render.bind(this))
@@ -30,52 +25,58 @@ class Renderer {
   }
 }
 const disconnector = {
-  renderers: [],
+  renderers: new Set(),
   isScheduled: false,
-  disconnect(...renderers) {
-    this.renderers.push(...renderers)
+  disconnect(renderers) {
+    renderers.forEach(renderer => this.renderers.add(renderer))
     if (!this.isScheduled) {
       this.isScheduled = true
       const clear = () => {
-        this.renderers.forEach(renderer => {
+        const renderersArray = [...this.renderers.values()]
+        renderersArray.forEach(renderer => {
           renderer.instances = renderer.instances.filter(instance => !instance.isUnmount)
         })
-        this.renderers.length = 0
+        this.renderers.clear()
         this.isScheduled = false
       }
       if (requestIdleCallback) {
         requestIdleCallback(clear)
       } else {
-        setTimeout(clear, 100);
+        setTimeout(clear, 250);
       }
     }
   }
 }
 Renderer.disconnect = disconnector.disconnect.bind(disconnector)
 
+const map = new Map()
 function createRenderer(store) {
-  if (createRenderer.map.has(store)) {
-    return createRenderer.map.get(store)
+  if (!(store instanceof Store)) {
+    throw new TypeError('Expected store to be an instance of Store.')
+  }
+  if (map.has(store)) {
+    return map.get(store)
   }
   const renderer = new Renderer(store)
-  createRenderer.map.set(store, renderer)
+  map.set(store, renderer)
   return renderer
 }
-createRenderer.map = new Map()
 
 class Prop {
   constructor(store, mapper = (state, updater) => Object.assign({}, state, updater)) {
-    this[$$renderer] = createRenderer(store)
+    const renderer = createRenderer(store)
     if (typeof mapper !== 'function') {
       throw new TypeError('Expected mapper to be a function.')
     }
     const updater = store.getUpdater()
-    this[$$value] = mapper(store.getState(), updater)
-    this[$$recalcs] = []
-    this[$$renderer].subscribe(state => {
-      this[$$value] = mapper(state, updater)
-      this[$$recalcs].forEach(recalc => recalc(this[$$value]))
+    const value = mapper(store.getState(), updater)
+    const calcs = []
+    const _private = {renderer, value, calcs}
+    renderer.subscribe(state => {
+      _private.value = mapper(state, updater)
+      calcs.forEach(calc => calc(_private.value))
     })
+    this[$$private] = _private
   }
 }
 
@@ -84,7 +85,7 @@ Prop.assign = function (...values) {
   const objects = []
   values.forEach(value => {
     if (value instanceof Prop) {
-      props.push(value)
+      props.push(value[$$private])
     } else if (typeof value === 'object') {
       objects.push(value)
     } else {
@@ -92,10 +93,13 @@ Prop.assign = function (...values) {
     }
   })
 
-  const value = Object.assign({}, ...objects, ...props.map(prop => prop[$$value]))
-  const recalc = subValue => Object.assign(value, subValue)
-  props.forEach(prop => prop[$$recalcs].push(recalc))
-  const renderers = props.map(prop => prop[$$renderer])
+  const value = Object.assign({}, ...objects, ...props.map(({value}) => value))
+  const calc = subValue => Object.assign(value, subValue)
+  const renderers = []
+  props.forEach(({calcs, renderer}) => {
+    calcs.push(calc)
+    renderers.push(renderer)
+  })
 
   return function (Component) {
     return class $Proper extends React.Component {
@@ -104,11 +108,10 @@ Prop.assign = function (...values) {
         this.isExpire = false
         this.isUnmount = false
         renderers.forEach(renderer => renderer.connect(this))
-        this.renderers = renderers
       }
       componentWillUnmount() {
         this.isUnmount = true
-        Renderer.disconnect(...this.renderers)
+        Renderer.disconnect(renderers)
       }
       update() {
         if (!this.isUnmount && this.isExpire) {
